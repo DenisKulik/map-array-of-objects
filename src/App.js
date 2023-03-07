21fcec4f21385cc76ca13101d1a17c6ea0cd3e8c
Denis Kulik <kulikdenis1996x@gmail.com>
2023-03-07 19:29:52 +0700

    display persons data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import persons from './data/persons';
+import Person from './components/Person';
 
 function App() {
-    return (<div className="App"></div>);
+    return (<div className="App">
+        {persons.map((person) => {
+            return <Person key={person.id} {...person}/>;
+        })}
+    </div>);
 }
 
 export default App;
